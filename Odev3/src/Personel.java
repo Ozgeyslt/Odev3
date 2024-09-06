dae1d49ab612fd42f5a82f5c9a07584a1d8dd9f0
Ozgeyslt <ozgey.saltan@gmail.com>
2024-09-06 19:04:43 +0300

    Odev 3

---
+++ Odev3/src/Personel.java
@@ -0,0 +1,17 @@
+public class Personel {
+    String name="Ayse";
+    String surname="Yilmaz";
+    int age=30;
+    String department="IT";
+    String adress="Istanbul/Turkiye";
+    int salary=20000;
+
+    public Personel(String name, String surname, int age, String department, String adress, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.department = department;
+        this.adress = adress;
+        this.salary = salary;
+    }
+}
